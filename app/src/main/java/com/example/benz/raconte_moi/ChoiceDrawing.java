85a4a64a8c15ea3f85d9b35a18446b49ab19a77d
mouna <maimouna93sow@gmail.com>
2017-03-12 12:51:50 +0100

    Check code style

--- app/src/main/java/com/example/benz/raconte_moi/ChoiceDrawing.java
+++ app/src/main/java/com/example/benz/raconte_moi/ChoiceDrawing.java
@@ -1,8 +1,8 @@
 package com.example.benz.raconte_moi;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -14,23 +14,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 public class ChoiceDrawing extends AppCompatActivity {
 
     private GridView gridView;
-    private GridViewAdapter gridAdapter;
 
     FirebaseStorage storage;
-    StorageReference storageRef;
-    FirebaseDatabase database;
+    //StorageReference storageRef;
+    //FirebaseDatabase database; 
     DatabaseReference refData;
 
 
     ImageItem it ;
-     ArrayList<ImageItem> imageItems;
+    ArrayList<ImageItem> imageItems;
     int i = 0;
 
 
@@ -40,8 +38,8 @@ public class ChoiceDrawing extends AppCompatActivity {
         setContentView(R.layout.activity_choice_drawing);
 
         storage = FirebaseStorage.getInstance();
-        storageRef = storage.getReference();
-        database = FirebaseDatabase.getInstance();
+        //storageRef = storage.getReference();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
         refData = database.getReference();
         gridView = (GridView) findViewById(R.id.gridHistory);
         imageItems = new ArrayList<>();
@@ -50,6 +48,7 @@ public class ChoiceDrawing extends AppCompatActivity {
         refData.child("history").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                GridViewAdapter gridAdapter;
 
                 gridAdapter = new GridViewAdapter(ChoiceDrawing.this, R.layout.grid_history,imageItems);
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
