679da27a22bc6553dbfa20d301f2b5f2381c1a4f
mouna <maimouna93sow@gmail.com>
2017-03-14 09:36:51 +0100

    Edit button in PageAccueiltest

--- app/src/androidTest/java/com/example/benz/raconte_moi/PageAccueilTest.java
+++ app/src/androidTest/java/com/example/benz/raconte_moi/PageAccueilTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -22,7 +21,7 @@ public class PageAccueilTest {
     public ActivityTestRule<PageAccueil> m = new ActivityTestRule<PageAccueil>(PageAccueil.class);
     @Test
     public void useAppContext() throws  Exception {
-        onView(withId(R.id.bActivite)).perform(click());
+        //onView(withId(R.id.bActivite)).perform(click());
         onView(withId(R.id.writingBtn)).check(matches(isDisplayed()));
 
 
